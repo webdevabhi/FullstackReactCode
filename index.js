0311568465b6336291372bc1e2a6cfc16099ccb5
webdevabhi <abhishek.patel185@gmail.com>
2018-08-29 15:04:16 +0530

    added script to package.json file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
     res.send({hi: 'there'});
 });
 
-app.listen(5000, () => {
-    console.log('Listening on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Listening on port ' + PORT);
 });
