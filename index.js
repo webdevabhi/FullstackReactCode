4f778ccd7d1744e0609352f9afa0bfaa1b137415
webdevabhi <abhishek.patel185@gmail.com>
2018-08-30 11:43:48 +0530

    Encoding Users

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require("./config/keys");
-require("./models/Users");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
@@ -9,6 +8,9 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 require("./routes/authRoutes")(app);
+app.route('/*').get(function (req, res) {
+    res.send({ status: 'OK' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
